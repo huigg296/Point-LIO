f873dd90b3bd553550dbb3a7534712c6589fdd02
LihanChen2004 <757003373@qq.com>
2024-09-17 18:04:20 +0800

    🎈 perf: refract launch file

--- launch/mapping_velody16.launch.py
+++ launch/mapping_velody16.launch.py
@@ -1,68 +1,97 @@
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
-from launch.actions import GroupAction, DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    # Declare the RViz argument
-    rviz_arg = DeclareLaunchArgument(
-        'rviz', default_value='true',
-        description='Flag to launch RViz.')
+    # Map fully qualified names to relative ones so the node's namespace can be prepended.
+    # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
+    # https://github.com/ros/geometry2/issues/32
+    # https://github.com/ros/robot_state_publisher/pull/30
+    # TODO(orduno) Substitute with `PushNodeRemapping`
+    #              https://github.com/ros2/launch_ros/issues/56
+    remappings = [("/tf", "tf"), ("/tf_static", "tf_static")]
 
-    # Node parameters, including those from the YAML configuration file
+    namespace = LaunchConfiguration("namespace")
+    use_rviz = LaunchConfiguration("rviz")
+    point_lio_cfg_dir = LaunchConfiguration("point_lio_cfg_dir")
+
+    point_lio_dir = get_package_share_directory("point_lio")
+
+    declare_namespace = DeclareLaunchArgument(
+        "namespace",
+        default_value="",
+        description="Namespace for the node",
+    )
+
+    declare_rviz = DeclareLaunchArgument(
+        "rviz", default_value="False", description="Flag to launch RViz."
+    )
+
+    declare_point_lio_cfg_dir = DeclareLaunchArgument(
+        "point_lio_cfg_dir",
+        default_value=PathJoinSubstitution([point_lio_dir, "config", "velody16.yaml"]),
+        description="Path to the Point-LIO config file",
+    )
+
     laser_mapping_params = [
-        PathJoinSubstitution([
-            FindPackageShare('point_lio'),
-            'config', 'velody16.yaml'
-        ]),
+        point_lio_cfg_dir,
         {
-            'use_imu_as_input': False,  # Change to True to use IMU as input of Point-LIO
-            'prop_at_freq_of_imu': True,
-            'check_satu': True,
-            'init_map_size': 10,
-            'point_filter_num': 4,  # Options: 4, 3
-            'space_down_sample': True,
-            'filter_size_surf': 0.5,  # Options: 0.5, 0.3, 0.2, 0.15, 0.1
-            'filter_size_map': 0.5,  # Options: 0.5, 0.3, 0.15, 0.1
-            'ivox_nearby_type': 6,  # Options: 0.5, 0.3, 0.15, 0.1
-            'runtime_pos_log_enable': False,  # Option: True
-        }
+            "use_imu_as_input": False,  # Change to True to use IMU as input of Point-LIO
+            "prop_at_freq_of_imu": True,
+            "check_satu": True,
+            "init_map_size": 10,
+            "point_filter_num": 4,  # Options: 4, 3
+            "space_down_sample": True,
+            "filter_size_surf": 0.5,  # Options: 0.5, 0.3, 0.2, 0.15, 0.1
+            "filter_size_map": 0.5,  # Options: 0.5, 0.3, 0.15, 0.1
+            "ivox_nearby_type": 6,  # Options: 0.5, 0.3, 0.15, 0.1
+            "runtime_pos_log_enable": False,  # Option: True
+        },
     ]
 
-    # Node definition for laserMapping with Point-LIO
-    laser_mapping_node = Node(
-        package='point_lio',
-        executable='pointlio_mapping',
-        name='laserMapping',
-        output='screen',
+    start_velodyne_convert_tool = Node(
+        package="isaac_sim_pointcloud_tool",
+        executable="converter",
+        namespace=namespace,
+        remappings=remappings,
+        output="screen",
+    )
+
+    start_laser_mapping_node = Node(
+        package="point_lio",
+        executable="pointlio_mapping",
+        namespace=namespace,
         parameters=laser_mapping_params,
-        # prefix='gdb -ex run --args'
+        remappings=remappings,
+        output="screen",
     )
 
-    # Conditional RViz node launch
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz',
-        arguments=['-d', PathJoinSubstitution([
-            FindPackageShare('point_lio'),
-            'rviz_cfg', 'loam_livox.rviz'
-        ])],
-        condition=IfCondition(LaunchConfiguration('rviz')),
-        prefix='nice'
+    start_rviz_node = Node(
+        condition=IfCondition(use_rviz),
+        package="rviz2",
+        executable="rviz2",
+        namespace=namespace,
+        name="rviz",
+        remappings=remappings,
+        arguments=[
+            "-d",
+            PathJoinSubstitution([point_lio_dir, "rviz_cfg", "loam_livox"]),
+            ".rviz",
+        ],
     )
 
-    # Assemble the launch description
-    ld = LaunchDescription([
-        rviz_arg,
-        laser_mapping_node,
-        GroupAction(
-            actions=[rviz_node],
-            condition=IfCondition(LaunchConfiguration('rviz'))
-        ),
-    ])
+    ld = LaunchDescription()
+
+    ld.add_action(declare_namespace)
+    ld.add_action(declare_rviz)
+    ld.add_action(declare_point_lio_cfg_dir)
+    ld.add_action(start_velodyne_convert_tool)
+    ld.add_action(start_laser_mapping_node)
+    ld.add_action(start_rviz_node)
 
     return ld
