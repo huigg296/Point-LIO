788214dbc6c128b04dd889e502101572e11b1ca9
LihanChen2004 <757003373@qq.com>
2024-09-19 22:34:40 +0800

    ✨ feat(point-lio-with-grid-map): Support ros2

--- include/IKFoM/IKFoM_toolkit/mtk/types/SOn.hpp
+++ include/IKFoM/IKFoM_toolkit/mtk/types/SOn.hpp
@@ -36,7 +36,7 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
- 
+
 /*
  *  Copyright (c) 2008--2011, Universitaet Bremen
  *  All rights reserved.
@@ -79,283 +79,279 @@
 
 #include <Eigen/Geometry>
 
-#include "vect.hpp"
 #include "../src/mtkmath.hpp"
+#include "vect.hpp"
 
-
-namespace MTK {
-
+namespace MTK
+{
 
 /**
  * Two-dimensional orientations represented as scalar.
  * There is no guarantee that the representing scalar is within any interval,
  * but the result of boxminus will always have magnitude @f$\le\pi @f$.
  */
-template<class _scalar = double, int Options = Eigen::AutoAlign>
-struct SO2 : public Eigen::Rotation2D<_scalar> {
-	enum {DOF = 1, DIM = 2, TYP = 3};
-	
-	typedef _scalar scalar;
-	typedef Eigen::Rotation2D<scalar> base;
-	typedef vect<DIM, scalar, Options> vect_type;
-	
-	//! Construct from angle
-	SO2(const scalar& angle = 0) : base(angle) {	}
-	
-	//! Construct from Eigen::Rotation2D
-	SO2(const base& src) : base(src) {}
-	
-	/**
+template <class _scalar = double, int Options = Eigen::AutoAlign>
+struct SO2 : public Eigen::Rotation2D<_scalar>
+{
+  enum { DOF = 1, DIM = 2, TYP = 3 };
+
+  typedef _scalar scalar;
+  typedef Eigen::Rotation2D<scalar> base;
+  typedef vect<DIM, scalar, Options> vect_type;
+
+  //! Construct from angle
+  SO2(const scalar & angle = 0) : base(angle) {}
+
+  //! Construct from Eigen::Rotation2D
+  SO2(const base & src) : base(src) {}
+
+  /**
 	 * Construct from 2D vector.
 	 * Resulting orientation will rotate the first unit vector to point to vec.
 	 */
-	SO2(const vect_type &vec) : base(atan2(vec[1], vec[0])) {};
-	
-	
-	//! Calculate @c this->inverse() * @c r
-	SO2 operator%(const base &r) const {
-		return base::inverse() * r;
-	}
+  SO2(const vect_type & vec) : base(atan2(vec[1], vec[0])){};
 
-	//! Calculate @c this->inverse() * @c r
-	template<class Derived>
-	vect_type operator%(const Eigen::MatrixBase<Derived> &vec) const {
-		return base::inverse() * vec;
-	}
-	
-	//! Calculate @c *this * @c r.inverse()
-	SO2 operator/(const SO2 &r) const {
-		return *this * r.inverse();
-	}
-	
-	//! Gets the angle as scalar.
-	operator scalar() const {
-		return base::angle(); 
-	}
-	void S2_hat(Eigen::Matrix<scalar, 3, 3> &res)
-	{
-		res = Eigen::Matrix<scalar, 3, 3>::Zero();
-	}
-	//! @name Manifold requirements
-	void S2_Nx_yy(Eigen::Matrix<scalar, 2, 3> &res)
-	{
-		std::cerr << "wrong idx for S2" << std::endl;
-		std::exit(100);	
-    	res = Eigen::Matrix<scalar, 2, 3>::Zero();
-	}
+  //! Calculate @c this->inverse() * @c r
+  SO2 operator%(const base & r) const { return base::inverse() * r; }
 
-	void S2_Mx(Eigen::Matrix<scalar, 3, 2> &res, MTK::vectview<const scalar, 2> delta)
-	{
-		std::cerr << "wrong idx for S2" << std::endl;
-		std::exit(100);	
-    	res = Eigen::Matrix<scalar, 3, 2>::Zero();
-	}
+  //! Calculate @c this->inverse() * @c r
+  template <class Derived>
+  vect_type operator%(const Eigen::MatrixBase<Derived> & vec) const
+  {
+    return base::inverse() * vec;
+  }
 
-	void oplus(MTK::vectview<const scalar, DOF> vec, scalar scale = 1) {
-		base::angle() += scale * vec[0];
-	}
-	
-	void boxplus(MTK::vectview<const scalar, DOF> vec, scalar scale = 1) {
-		base::angle() += scale * vec[0];
-	}
-	void boxminus(MTK::vectview<scalar, DOF> res, const SO2<scalar>& other) const {
-		res[0] = MTK::normalize(base::angle() - other.angle(), scalar(MTK::pi));
-	}
-	
-	friend std::istream& operator>>(std::istream &is, SO2<scalar>& ang){
-		return is >> ang.angle();
-	}
-	void hat(Eigen::VectorXd& v, Eigen::MatrixXd &res) {
-		std::cout << "wrong idx" << std::endl;
-	}
-	void Jacob_right_inv(Eigen::VectorXd& v, Eigen::MatrixXd &res) {
-		std::cout << "wrong idx" << std::endl;
-	}
-	void Jacob_right(Eigen::VectorXd& v, Eigen::MatrixXd &res) {
-		std::cout << "wrong idx" << std::endl;
-	}
+  //! Calculate @c *this * @c r.inverse()
+  SO2 operator/(const SO2 & r) const { return *this * r.inverse(); }
+
+  //! Gets the angle as scalar.
+  operator scalar() const { return base::angle(); }
+  void S2_hat(Eigen::Matrix<scalar, 3, 3> & res) { res = Eigen::Matrix<scalar, 3, 3>::Zero(); }
+  //! @name Manifold requirements
+  void S2_Nx_yy(Eigen::Matrix<scalar, 2, 3> & res)
+  {
+    std::cerr << "wrong idx for S2" << '\n';
+    std::exit(100);
+    res = Eigen::Matrix<scalar, 2, 3>::Zero();
+  }
+
+  void S2_Mx(Eigen::Matrix<scalar, 3, 2> & res, MTK::vectview<const scalar, 2> delta)
+  {
+    std::cerr << "wrong idx for S2" << '\n';
+    std::exit(100);
+    res = Eigen::Matrix<scalar, 3, 2>::Zero();
+  }
+
+  void oplus(MTK::vectview<const scalar, DOF> vec, scalar scale = 1)
+  {
+    base::angle() += scale * vec[0];
+  }
+
+  void boxplus(MTK::vectview<const scalar, DOF> vec, scalar scale = 1)
+  {
+    base::angle() += scale * vec[0];
+  }
+  void boxminus(MTK::vectview<scalar, DOF> res, const SO2<scalar> & other) const
+  {
+    res[0] = MTK::normalize(base::angle() - other.angle(), scalar(MTK::pi));
+  }
+
+  friend std::istream & operator>>(std::istream & is, SO2<scalar> & ang)
+  {
+    return is >> ang.angle();
+  }
+  void hat(Eigen::VectorXd & v, Eigen::MatrixXd & res) { std::cout << "wrong idx" << '\n'; }
+  void Jacob_right_inv(Eigen::VectorXd & v, Eigen::MatrixXd & res)
+  {
+    std::cout << "wrong idx" << '\n';
+  }
+  void Jacob_right(Eigen::VectorXd & v, Eigen::MatrixXd & res) { std::cout << "wrong idx" << '\n'; }
 };
-
 
 /**
  * Three-dimensional orientations represented as Quaternion.
  * It is assumed that the internal Quaternion always stays normalized,
  * should this not be the case, call inherited member function @c normalize().
  */
-template<class _scalar = double> //, int Options = Eigen::AutoAlign>
-struct SO3 : public Eigen::Matrix<_scalar, 3, 3> {
-	enum {DOF = 3, DIM = 3, TYP = 2}; 
-	typedef _scalar scalar;
-	typedef Eigen::Matrix<scalar, 3, 3> base;
-	typedef Eigen::Matrix<scalar, 3, 3> Matrix;
-	typedef vect<DIM, scalar> vect_type;
-	
-	/**
+template <class _scalar = double>  //, int Options = Eigen::AutoAlign>
+struct SO3 : public Eigen::Matrix<_scalar, 3, 3>
+{
+  enum { DOF = 3, DIM = 3, TYP = 2 };
+  typedef _scalar scalar;
+  typedef Eigen::Matrix<scalar, 3, 3> base;
+  typedef Eigen::Matrix<scalar, 3, 3> Matrix;
+  typedef vect<DIM, scalar> vect_type;
+
+  /**
 	 * Construct from Eigen::Quaternion.
 	 * @note Non-normalized input may result result in spurious behavior.
 	 */
-	SO3(const base& src = base::Identity()) : base(src) {}
-	
-	/**
+  SO3(const base & src = base::Identity()) : base(src) {}
+
+  /**
 	 * Construct from rotation matrix.
 	 * @note Invalid rotation matrices may lead to spurious behavior.
 	 */
-	template<class Derived>
-	SO3(const Eigen::MatrixBase<Derived>& matrix) : base(matrix) {}
-	
-	/**
+  template <class Derived>
+  SO3(const Eigen::MatrixBase<Derived> & matrix) : base(matrix)
+  {
+  }
+
+  /**
 	 * Construct from arbitrary rotation type.
 	 * @note Invalid rotation matrices may lead to spurious behavior.
 	 */
-	// template<class Derived>
-	// SO3(const Eigen::RotationBase<Derived, 3>& rotation) : base(rotation.derived()) {}
-	
-	//! @name Manifold requirements
-	
-	// SO3 operator=(const base &r) const {
-	// 	return r;
-	// }
+  // template<class Derived>
+  // SO3(const Eigen::RotationBase<Derived, 3>& rotation) : base(rotation.derived()) {}
 
-	// //! Calculate @c *this * @c r.inverse()
-	// SO3 operator*(const SO3 &r) const {
-	// 	return *this * r;
-	// }
+  //! @name Manifold requirements
 
-	void boxplus(MTK::vectview<const scalar, DOF> vec, scalar scale=1) {
-		SO3 delta = exp(vec, scale);
-		*this = *this * delta;
-	}
-	void boxminus(MTK::vectview<scalar, DOF> res, const SO3<scalar>& other) const {
-		res = SO3::log(other.transpose() * *this);
-	}
-	//}
+  // SO3 operator=(const base &r) const {
+  // 	return r;
+  // }
 
-	void oplus(MTK::vectview<const scalar, DOF> vec, scalar scale=1) {
-		SO3 delta = exp(vec, scale);
-		*this = *this * delta;
-	}
+  // //! Calculate @c *this * @c r.inverse()
+  // SO3 operator*(const SO3 &r) const {
+  // 	return *this * r;
+  // }
 
-	// void hat(MTK::vectview<const scalar, DOF>& v, Eigen::Matrix<scalar, 3, 3> &res) {
-	void hat(Eigen::VectorXd& v, Eigen::MatrixXd &res) {
-		// Eigen::Matrix<scalar, 3, 3> res;
-		res << 0, -v[2], v[1],
-			v[2], 0, -v[0],
-			-v[1], v[0], 0;
-		// return res;
-	}
+  void boxplus(MTK::vectview<const scalar, DOF> vec, scalar scale = 1)
+  {
+    SO3 delta = exp(vec, scale);
+    *this = *this * delta;
+  }
+  void boxminus(MTK::vectview<scalar, DOF> res, const SO3<scalar> & other) const
+  {
+    res = SO3::log(other.transpose() * *this);
+  }
+  //}
 
-	// void Jacob_right_inv(MTK::vectview<const scalar, DOF> vec, Eigen::Matrix<scalar, 3, 3> & res){
-	void Jacob_right_inv(Eigen::VectorXd& vec, Eigen::MatrixXd &res){
-    	Eigen::MatrixXd hat_v;
-		hat(vec, hat_v);
-    	if(vec.norm() > MTK::tolerance<scalar>())
-    	{
-        	res = Eigen::Matrix<scalar, 3, 3>::Identity() + 0.5 * hat_v + (1 - vec.norm() * std::cos(vec.norm() / 2) / 2 / std::sin(vec.norm() / 2)) * hat_v * hat_v / vec.squaredNorm();
-    	}
-    	else
-    	{
-        	res = Eigen::Matrix<scalar, 3, 3>::Identity();
-    	}
-    	// return res;
-	}
+  void oplus(MTK::vectview<const scalar, DOF> vec, scalar scale = 1)
+  {
+    SO3 delta = exp(vec, scale);
+    *this = *this * delta;
+  }
 
-	// void Jacob_right(MTK::vectview<const scalar, DOF> & v, Eigen::Matrix<scalar, 3, 3> &res){
-	void Jacob_right(Eigen::VectorXd& v, Eigen::MatrixXd &res){
-		Eigen::MatrixXd hat_v;
-		hat(v, hat_v);
-		double squaredNorm = v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
-		double norm = std::sqrt(squaredNorm);
-		if(norm < MTK::tolerance<scalar>()){
-			res = Eigen::Matrix<scalar, 3, 3>::Identity();
-		}
-		else{
-			res = Eigen::Matrix<scalar, 3, 3>::Identity() - (1 - std::cos(norm)) / squaredNorm * hat_v + (1 - std::sin(norm) / norm) / squaredNorm * hat_v * hat_v;
-		}
-		// return res;
-	}
+  // void hat(MTK::vectview<const scalar, DOF>& v, Eigen::Matrix<scalar, 3, 3> &res) {
+  void hat(Eigen::VectorXd & v, Eigen::MatrixXd & res)
+  {
+    // Eigen::Matrix<scalar, 3, 3> res;
+    res << 0, -v[2], v[1], v[2], 0, -v[0], -v[1], v[0], 0;
+    // return res;
+  }
 
-	void S2_hat(Eigen::Matrix<scalar, 3, 3> &res)
-	{
-		res = Eigen::Matrix<scalar, 3, 3>::Zero();
-	}
-	void S2_Nx_yy(Eigen::Matrix<scalar, 2, 3> &res)
-	{
-		std::cerr << "wrong idx for S2" << std::endl;
-		std::exit(100);	
-    	res = Eigen::Matrix<scalar, 2, 3>::Zero();
-	}
+  // void Jacob_right_inv(MTK::vectview<const scalar, DOF> vec, Eigen::Matrix<scalar, 3, 3> & res){
+  void Jacob_right_inv(Eigen::VectorXd & vec, Eigen::MatrixXd & res)
+  {
+    Eigen::MatrixXd hat_v;
+    hat(vec, hat_v);
+    if (vec.norm() > MTK::tolerance<scalar>()) {
+      res = Eigen::Matrix<scalar, 3, 3>::Identity() + 0.5 * hat_v +
+            (1 - vec.norm() * std::cos(vec.norm() / 2) / 2 / std::sin(vec.norm() / 2)) * hat_v *
+              hat_v / vec.squaredNorm();
+    } else {
+      res = Eigen::Matrix<scalar, 3, 3>::Identity();
+    }
+    // return res;
+  }
 
-	void S2_Mx(Eigen::Matrix<scalar, 3, 2> &res, MTK::vectview<const scalar, 2> delta)
-	{
-		std::cerr << "wrong idx for S2" << std::endl;
-		std::exit(100);	
-    	res = Eigen::Matrix<scalar, 3, 2>::Zero();
-	}
+  // void Jacob_right(MTK::vectview<const scalar, DOF> & v, Eigen::Matrix<scalar, 3, 3> &res){
+  void Jacob_right(Eigen::VectorXd & v, Eigen::MatrixXd & res)
+  {
+    Eigen::MatrixXd hat_v;
+    hat(v, hat_v);
+    double squaredNorm = v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
+    double norm = std::sqrt(squaredNorm);
+    if (norm < MTK::tolerance<scalar>()) {
+      res = Eigen::Matrix<scalar, 3, 3>::Identity();
+    } else {
+      res = Eigen::Matrix<scalar, 3, 3>::Identity() - (1 - std::cos(norm)) / squaredNorm * hat_v +
+            (1 - std::sin(norm) / norm) / squaredNorm * hat_v * hat_v;
+    }
+    // return res;
+  }
 
-	friend std::ostream& operator<<(std::ostream &os, const SO3<scalar>& q){ // wrong!
-		return os << q.data() << " ";
-	}
+  void S2_hat(Eigen::Matrix<scalar, 3, 3> & res) { res = Eigen::Matrix<scalar, 3, 3>::Zero(); }
+  void S2_Nx_yy(Eigen::Matrix<scalar, 2, 3> & res)
+  {
+    std::cerr << "wrong idx for S2" << '\n';
+    std::exit(100);
+    res = Eigen::Matrix<scalar, 2, 3>::Zero();
+  }
 
-	friend std::istream& operator>>(std::istream &is, SO3<scalar>& q){ // wrong!
-		SO3<scalar> coeffs;
-		is >> coeffs;
-		q = coeffs;
-		return is;
-	}
-	
-	//! @name Helper functions
-	//{
-	/**
+  void S2_Mx(Eigen::Matrix<scalar, 3, 2> & res, MTK::vectview<const scalar, 2> delta)
+  {
+    std::cerr << "wrong idx for S2" << '\n';
+    std::exit(100);
+    res = Eigen::Matrix<scalar, 3, 2>::Zero();
+  }
+
+  friend std::ostream & operator<<(std::ostream & os, const SO3<scalar> & q)
+  {  // wrong!
+    return os << q.data() << " ";
+  }
+
+  friend std::istream & operator>>(std::istream & is, SO3<scalar> & q)
+  {  // wrong!
+    SO3<scalar> coeffs;
+    is >> coeffs;
+    q = coeffs;
+    return is;
+  }
+
+  //! @name Helper functions
+  //{
+  /**
 	 * Calculate the exponential map. In matrix terms this would correspond 
 	 * to the Rodrigues formula.
 	 */
-	// FIXME vectview<> can't be constructed from every MatrixBase<>, use const Vector3x& as workaround
-//	static SO3 exp(MTK::vectview<const scalar, 3> dvec, scalar scale = 1){
-	static SO3 exp(const Eigen::Matrix<scalar, 3, 1>& dvec, scalar scale = 1){
-		Eigen::Matrix<scalar, 3, 1> ang = dvec * scale;
-		double ang_norm = ang.norm();
-    	Eigen::Matrix<double, 3, 3> Eye3 = Eigen::Matrix<double, 3, 3>::Identity();
-    	if (ang_norm > 0.0000001)
-    	{
-        	Eigen::Matrix<double, 3, 1> r_axis = ang / ang_norm;
-        	Eigen::Matrix<double, 3, 3> K;
-        	K << 0.0, -r_axis(2), r_axis(1),
-				r_axis(2), 0.0, -r_axis(0),
-				-r_axis(1), r_axis(0), 0.0; // SKEW_SYM_MATRX(r_axis);
-        	/// Roderigous Tranformation
-        	return Eye3 + std::sin(ang_norm) * K + (1.0 - std::cos(ang_norm)) * K * K;
-    	}
-    	else
-    	{
-        	return Eye3;
-    	}
-	}
-	/**
+  // FIXME vectview<> can't be constructed from every MatrixBase<>, use const Vector3x& as workaround
+  //	static SO3 exp(MTK::vectview<const scalar, 3> dvec, scalar scale = 1){
+  static SO3 exp(const Eigen::Matrix<scalar, 3, 1> & dvec, scalar scale = 1)
+  {
+    Eigen::Matrix<scalar, 3, 1> ang = dvec * scale;
+    double ang_norm = ang.norm();
+    Eigen::Matrix<double, 3, 3> Eye3 = Eigen::Matrix<double, 3, 3>::Identity();
+    if (ang_norm > 0.0000001) {
+      Eigen::Matrix<double, 3, 1> r_axis = ang / ang_norm;
+      Eigen::Matrix<double, 3, 3> K;
+      K << 0.0, -r_axis(2), r_axis(1), r_axis(2), 0.0, -r_axis(0), -r_axis(1), r_axis(0),
+        0.0;  // SKEW_SYM_MATRX(r_axis);
+      /// Roderigous Tranformation
+      return Eye3 + std::sin(ang_norm) * K + (1.0 - std::cos(ang_norm)) * K * K;
+    } else {
+      return Eye3;
+    }
+  }
+  /**
 	 * Calculate the inverse of @c exp.
 	 * Only guarantees that <code>exp(log(x)) == x </code>
 	 */
-	static Eigen::Vector3d log(const SO3 &orient){
-		double theta = (orient.trace() > 3.0 - 1e-6) ? 0.0 : std::acos(0.5 * (orient.trace() - 1));
-    	Eigen::Matrix<double,3,1> K(orient(2,1) - orient(1,2), orient(0,2) - orient(2,0), orient(1,0) - orient(0,1));
-    	return (std::abs(theta) < 0.001) ? (0.5 * K) : (0.5 * theta / std::sin(theta) * K);
-	}
+  static Eigen::Vector3d log(const SO3 & orient)
+  {
+    double theta = (orient.trace() > 3.0 - 1e-6) ? 0.0 : std::acos(0.5 * (orient.trace() - 1));
+    Eigen::Matrix<double, 3, 1> K(
+      orient(2, 1) - orient(1, 2), orient(0, 2) - orient(2, 0), orient(1, 0) - orient(0, 1));
+    return (std::abs(theta) < 0.001) ? (0.5 * K) : (0.5 * theta / std::sin(theta) * K);
+  }
 };
 
-namespace internal {
-template<class Scalar, int Options>
-struct UnalignedType<SO2<Scalar, Options > >{
-	typedef SO2<Scalar, Options | Eigen::DontAlign> type;
+namespace internal
+{
+template <class Scalar, int Options>
+struct UnalignedType<SO2<Scalar, Options> >
+{
+  typedef SO2<Scalar, Options | Eigen::DontAlign> type;
 };
 
-template<class Scalar>
-struct UnalignedType<SO3<Scalar> >{
-	typedef SO3<Scalar> type;
+template <class Scalar>
+struct UnalignedType<SO3<Scalar> >
+{
+  typedef SO3<Scalar> type;
 };
 
 }  // namespace internal
 
-
 }  // namespace MTK
 
 #endif /*SON_H_*/
-
--- src/preprocess.h
+++ src/preprocess.h
@@ -1,27 +1,29 @@
-#include <ros/ros.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <livox_ros_driver/CustomMsg.h>
+
+#include <deque>
+#include <livox_ros_driver2/msg/custom_msg.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 
 using namespace std;
 
-#define IS_VALID(a)  ((abs(a)>1e8) ? true : false)
+#define IS_VALID(a) ((abs(a) > 1e8) ? true : false)
 
 typedef pcl::PointXYZINormal PointType;
 typedef pcl::PointCloud<PointType> PointCloudXYZI;
 
-enum LID_TYPE{AVIA = 1, VELO16, OUST64, HESAIxt32}; //{1, 2, 3, 4}
-enum TIME_UNIT{SEC = 0, MS = 1, US = 2, NS = 3};
-enum Feature{Nor, Poss_Plane, Real_Plane, Edge_Jump, Edge_Plane, Wire, ZeroPoint};
-enum Surround{Prev, Next};
-enum E_jump{Nr_nor, Nr_zero, Nr_180, Nr_inf, Nr_blind};
+enum LID_TYPE { AVIA = 1, VELO16, OUST64, HESAIxt32 };  //{1, 2, 3, 4}
+enum TIME_UNIT { SEC = 0, MS = 1, US = 2, NS = 3 };
+enum Feature { Nor, Poss_Plane, Real_Plane, Edge_Jump, Edge_Plane, Wire, ZeroPoint };
+enum Surround { Prev, Next };
+enum E_jump { Nr_nor, Nr_zero, Nr_180, Nr_inf, Nr_blind };
 
-const bool time_list_cut_frame(PointType &x, PointType &y);
+const bool time_list_cut_frame(PointType & x, PointType & y);
 
 struct orgtype
 {
   double range;
-  double dista; 
+  double dista;
   double angle[2];
   double intersect;
   E_jump edj[2];
@@ -36,53 +38,49 @@ struct orgtype
   }
 };
 
-namespace velodyne_ros {
-  struct EIGEN_ALIGN16 Point {
-      PCL_ADD_POINT4D;
-      float intensity;
-      float time;
-      uint16_t ring;
-      EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-  };
+namespace velodyne_ros
+{
+struct EIGEN_ALIGN16 Point
+{
+  PCL_ADD_POINT4D;
+  float intensity;
+  float time;
+  uint16_t ring;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
 }  // namespace velodyne_ros
-POINT_CLOUD_REGISTER_POINT_STRUCT(velodyne_ros::Point,
-    (float, x, x)
-    (float, y, y)
-    (float, z, z)
-    (float, intensity, intensity)
-    (float, time, time)
-    (std::uint16_t, ring, ring)
-)
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+  velodyne_ros::Point, (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(
+                         float, time, time)(std::uint16_t, ring, ring))
 
-namespace hesai_ros {
-  struct EIGEN_ALIGN16 Point {
-      PCL_ADD_POINT4D;
-      float intensity;
-      double timestamp;
-      uint16_t ring;
-      EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-  };
-}  // namespace velodyne_ros
-POINT_CLOUD_REGISTER_POINT_STRUCT(hesai_ros::Point,
-    (float, x, x)
-    (float, y, y)
-    (float, z, z)
-    (float, intensity, intensity)
-    (double, timestamp, timestamp)
-    (std::uint16_t, ring, ring)
-)
+namespace hesai_ros
+{
+struct EIGEN_ALIGN16 Point
+{
+  PCL_ADD_POINT4D;
+  float intensity;
+  double timestamp;
+  uint16_t ring;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+}  // namespace hesai_ros
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+  hesai_ros::Point, (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(
+                      double, timestamp, timestamp)(std::uint16_t, ring, ring))
 
-namespace ouster_ros {
-  struct EIGEN_ALIGN16 Point {
-      PCL_ADD_POINT4D;
-      float intensity;
-      uint32_t t;
-      uint16_t reflectivity;
-      uint8_t  ring;
-      uint16_t ambient;
-      uint32_t range;
-      EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-  };
+namespace ouster_ros
+{
+struct EIGEN_ALIGN16 Point
+{
+  PCL_ADD_POINT4D;
+  float intensity;
+  uint32_t t;
+  uint16_t reflectivity;
+  uint8_t ring;
+  uint16_t ambient;
+  uint32_t range;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
 }  // namespace ouster_ros
 
 // clang-format off
@@ -107,15 +105,15 @@ class Preprocess
   Preprocess();
   ~Preprocess();
   
-  void process_cut_frame_livox(const livox_ros_driver::CustomMsg::ConstPtr &msg, deque<PointCloudXYZI::Ptr> &pcl_out, deque<double> &time_lidar, const int required_frame_num, int scan_count);
+  void process_cut_frame_livox(const livox_ros_driver2::msg::CustomMsg::SharedPtr &msg, deque<PointCloudXYZI::Ptr> &pcl_out, deque<double> &time_lidar, const int required_frame_num, int scan_count);
   
-  void process_cut_frame_pcl2(const sensor_msgs::PointCloud2::ConstPtr &msg, deque<PointCloudXYZI::Ptr> &pcl_out, deque<double> &time_lidar, const int required_frame_num, int scan_count);
+  void process_cut_frame_pcl2(const sensor_msgs::msg::PointCloud2::SharedPtr &msg, deque<PointCloudXYZI::Ptr> &pcl_out, deque<double> &time_lidar, const int required_frame_num, int scan_count);
  
-  void process(const livox_ros_driver::CustomMsg::ConstPtr &msg, PointCloudXYZI::Ptr &pcl_out);
-  void process(const sensor_msgs::PointCloud2::ConstPtr &msg, PointCloudXYZI::Ptr &pcl_out);
+  void process(const livox_ros_driver2::msg::CustomMsg::SharedPtr &msg, PointCloudXYZI::Ptr &pcl_out);
+  void process(const sensor_msgs::msg::PointCloud2::SharedPtr &msg, PointCloudXYZI::Ptr &pcl_out);
   void set(bool feat_en, int lid_type, double bld, int pfilt_num);
 
-  // sensor_msgs::PointCloud2::ConstPtr pointcloud;
+  // sensor_msgs::msg::PointCloud2::SharedPtr pointcloud;
   PointCloudXYZI pl_full, pl_corn, pl_surf;
   PointCloudXYZI pl_buff[128]; //maximum 128 line lidar
   vector<orgtype> typess[128]; //maximum 128 line lidar
@@ -123,16 +121,13 @@ class Preprocess
   int lidar_type, point_filter_num, N_SCANS, SCAN_RATE, time_unit;
   double blind, det_range;
   bool given_offset_time;
-  ros::Publisher pub_full, pub_surf, pub_corn;
-    
 
   private:
-  void avia_handler(const livox_ros_driver::CustomMsg::ConstPtr &msg);
-  void oust64_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
-  void velodyne_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
-  void hesai_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
+  void avia_handler(const livox_ros_driver2::msg::CustomMsg::SharedPtr &msg);
+  void oust64_handler(const sensor_msgs::msg::PointCloud2::SharedPtr &msg);
+  void velodyne_handler(const sensor_msgs::msg::PointCloud2::SharedPtr &msg);
+  void hesai_handler(const sensor_msgs::msg::PointCloud2::SharedPtr &msg);
   void give_feature(PointCloudXYZI &pl, vector<orgtype> &types);
-  void pub_func(PointCloudXYZI &pl, const ros::Time &ct);
   int  plane_judge(const PointCloudXYZI &pl, vector<orgtype> &types, uint i, uint &i_nex, Eigen::Vector3d &curr_direct);
   bool small_plane(const PointCloudXYZI &pl, vector<orgtype> &types, uint i_cur, uint &i_nex, Eigen::Vector3d &curr_direct);
   bool edge_jump_judge(const PointCloudXYZI &pl, vector<orgtype> &types, uint i, Surround nor_dir);
